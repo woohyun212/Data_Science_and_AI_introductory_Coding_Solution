ac77bade4c301953d487c7e83a72294ac91c5865
woohyun212 <woohyun212@gmail.com>
2021-04-26 20:57:19 +0900

    123python 과 같은 경우 처리헐 수 있게 수정

--- chapter9/ch_5.py
+++ chapter9/ch_5.py
@@ -1,12 +1,12 @@
 import re
 
-text = '''101 COM Python Part1
+text = '''101 COM 123Python Part1
 102 MAT LinearAlgebra
 103 ENG ComputerEnglish'''
 s = []
 for word in text.split():
-    code = re.search("^[A-Z][A-Z][A-Z]+", word)
+    code = re.search("^\d*$", word)
     if code:  # None 이 아니면
         s.append(code.group())
 print(s)
-# 맘에 안듬 만약에 123python 의 경우에는 어떻게 처리하지?
+
