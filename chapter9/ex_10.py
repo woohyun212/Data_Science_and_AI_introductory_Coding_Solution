c6c6ed010839ed7e0331c75fe96676d3d4f253dd
woohyun212 <woohyun212@gmail.com>
2021-05-04 01:19:21 +0900

    9 chapter 심화문제 풀이

--- chapter9/ex_10.py
+++ chapter9/ex_10.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import wikipedia
 from wordcloud import WordCloud, STOPWORDS
 
-wiki = wikipedia.page('korea')
-text = wiki.content
-s_word = STOPWORDS.union({'south', 'north', 'korean', 'world', 'country'})
+text = wikipedia.page('korea').content + wikipedia.page('Japan').content
+s_word = STOPWORDS.union({'south', 'north', 'korean', 'world', 'country', 'one'})
 wordcloud1 = WordCloud(width=1280, height=720, stopwords=s_word).generate(text)
 plt.figure(figsize=(20, 10))
 plt.imshow(wordcloud1)
